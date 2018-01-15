fc4ac4e3b7a2c675c436144722b29b625e5cdde2
sandy.fang <751541230@qq.com>
2018-01-15 09:26:59 +0800

    Update.

--- install.sh
+++ install.sh
@@ -8,19 +8,12 @@ set -e
 
 LINUX_CONFIG_PATH=$(dirname $(readlink -f $0))
 
-ins_ssh()
-{
-    sudo apt-get install openssh-server
-    sudo service ssh start
-}
-
 ins_samba()
 {
+    echo "Now execute ins_samba."
     sudo apt-get install samba
     sudo apt-get install smbclient
     sudo /etc/init.d/samba start
-    sudo /etc/init.d/samba stop
-    sudo /etc/init.d/samba restart
     samba_dir=$HOME/pub/
     if [ ! -d $samba_dir ]; then
         mkdir $samba_dir
@@ -48,6 +41,7 @@ ins_samba()
 
 ins_python()
 {
+    echo "Now execute ins_python."
     sudo apt-get install -y python python-dev python-pip
     sudo apt-get install -y python3 python-dev python3-pip
     make -C $LINUX_CONFIG_PATH install-pip
@@ -60,6 +54,7 @@ ins_python()
 
 ins_nvim()
 {
+    echo "Now execute ins_nvim."
     sudo apt-get install -y software-properties-common
     sudo add-apt-repository ppa:neovim-ppa/stable
     sudo apt-get install -y neovim
@@ -83,6 +78,7 @@ ins_nvim()
 
 ins_zsh()
 {
+    echo "Now execute ins_zsh."
     # 注意：在init函数中， 该函数必须在最后执行，由于执行完终端会切换到zsh，从而终端脚本
     sudo apt-get install -y zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -90,6 +86,7 @@ ins_zsh()
 
 ins_fzf()
 {
+    echo "Now execute ins_fzf."
     if [[ -e ~/.fzf ]]; then
         (cd ~/.fzf; git pull)
     else
@@ -100,17 +97,20 @@ ins_fzf()
 
 ins_other()
 {
+    echo "Now execute ins_other."
     # common
     sudo apt-get install -y vim openssh-client openssh-server lrzsz
     # zsh plugin
     sudo apt-get install -y autojump silversearcher-ag
     # nvim
     sudo apt-get install -y exuberant-ctags
+    sudo service ssh start
 }
 
 # 安装nvim插件配置
 ins_nvim_plug_conf()
 {
+    echo "Now execute ins_nvim_plug_conf."
     py_snip=$LINUX_CONFIG_PATH/nvim/plugged/vim-snippets/UltiSnips/python.snippets
     py_snip_bak=$LINUX_CONFIG_PATH/nvim/plugged/vim-snippets/UltiSnips/python.snippets.bak
     diy_py_snip=$LINUX_CONFIG_PATH/nvim/python.snippets
@@ -126,8 +126,8 @@ ins_nvim_plug_conf()
 # 安装用于nvim的python插件
 ins_pytools()
 {
+    echo "Now execute ins_pytools."
     # for nvim
-    echo "now install python for vim."
     pip install -U pip neovim jedi flake8 pep8 pylint
 
     # tools
@@ -143,9 +143,10 @@ ins_pytools()
 # 用于装完系统后安装各类工具
 init()
 {
+    echo "Now execute apt-get update."
     sudo apt-get update
+    echo "Now install git."
     sudo apt-get install git
-    ins_ssh
     ins_samba
     ins_python
     ins_nvim
@@ -160,6 +161,7 @@ init()
 # 该函数必须在zsh安装完后安装，由于该函数会生成.oh-my-zsh目录，导致ins_zsh无法正常安装
 ins_zsh_plug()
 {
+    echo "Now execute ins_zsh_plug."
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestion
     make -C $LINUX_CONFIG_PATH install-zsh
@@ -170,11 +172,11 @@ help()
     cat << EOF
 Usage:            ./install [OPT]
 OPT:
-    ins_ssh:            安装open-ssh
     ins_samba:          安装samba及相关配置，共享目录为~/pub
     ins_python:         安装python以及虚拟环境
     ins_nvim:           安装nvim以及相关插件ins_nvim_plug_conf
     ins_fzf:            安装fzf
+    ins_other:          安装open-ssh, autojump, ctags, vim等
     ins_pytools:        安装python工具，在安装python虚拟环境后安装
     ins_zsh:            安装zsh
     init:               执行ins_ssh, ins_samba, ins_python, ins_nvim, ins_fzf, ins_other, make, ins_pytools, ins_zsh
